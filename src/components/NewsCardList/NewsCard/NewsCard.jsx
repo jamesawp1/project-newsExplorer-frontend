ffa282db0f4430355925ce7b2d87b02374d284c6
jamesawp1 <jamesawp@protonmail.com>
2025-12-01 10:17:38 -0300

    fix: Fix the date format

--- src/components/NewsCardList/NewsCard/NewsCard.jsx
+++ src/components/NewsCardList/NewsCard/NewsCard.jsx
@@ -2,12 +2,18 @@ import image from "../../../images/image_05.png";
 
 export default function NewsCard(props) {
   const { data } = props;
+  console.log(data);
+  const date = new Date(data.publishedAt).toLocaleDateString("pt-BR", {
+    day: "2-digit",
+    month: "long",
+    year: "numeric",
+  });
 
   return (
     <li className="news__card">
       <img className="news__card-image" src={data.urlToImage} />
       <div className="news__card-wrapper_text">
-        <p className="news__card-text_data">{data.publishedAt}</p>
+        <p className="news__card-text_data">{date}</p>
         <h3 className="news__card-text_title">{data.title}</h3>
         <p className="news__card-text_paragraph">{data.description}</p>
         <p className="news__card-text_source">{data.source.name}</p>
