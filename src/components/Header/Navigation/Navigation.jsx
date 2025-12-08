bce46367d371539c43050ade71832e05bef022bb
jamesawp1 <jamesawp@protonmail.com>
2025-12-08 10:29:06 -0300

    feat: Add header mobile layout

--- src/components/Header/Navigation/Navigation.jsx
+++ src/components/Header/Navigation/Navigation.jsx
@@ -1,7 +1,12 @@
+import hamburguer from "../../../images/hamburguer.png";
+
 export default function Navigation() {
   return (
     <div className="header__navigation">
       <h1 className="header__navigation_title">News Explorer</h1>
+      <button className="header__navigation_hamburguer">
+        <img className="header__navigation_hamburguer-icon" src={hamburguer} />
+      </button>
       <div className="header__navigation_wrapper">
         <button className="header__navigation_button">Início</button>
         <button
